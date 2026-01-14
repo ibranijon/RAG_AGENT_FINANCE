77ca39407ea39f9ba8629b156adcd3ad2c4e657c
Jon Ibrani <ibranijon@gmail.com>
2026-01-14 11:02:03 +0100

    retrieval_grader

---
+++ graph/chains/tests/test_chains.py
@@ -0,0 +1,26 @@
+from ingestion_retrival import retriever
+from graph.chains.retrieval_grader import retrieval_grader, GradeDocuments
+
+
+def test_retrival_grader_answer_yes() -> None:
+    question = "AI in finance"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+
+def test_retrival_grader_answer_no() -> None:
+    question = "How to make pizza like Tony Soprano"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
